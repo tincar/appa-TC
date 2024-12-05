d57ef210c5f5cbdeec0f0c87874d6dcb187cd335
tincar <tin.car543@gmail.com>
2024-12-05 10:30:32 +0100

    Kolokvij

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>"Provjera ishoda 1 i 2"</h2>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
